46043feb1032c1927c23daf22389883b4ba8f002
DEGUCHIHIROKI <deguhiro4062@gmail.com>
2024-04-25 01:10:55 +0900

    問題1のみjsを編集、適用(固定の答えの時正解を判別し、正しい挙動をが出る)

---
+++ Web/Quiz/questions.js
@@ -0,0 +1,42 @@
+const choicesList = document.querySelectorAll('ol.choices li');
+
+choicesList.forEach(li => li.addEventListener('click', checkClickedChoice));
+
+function checkClickedChoice(event) {
+    // クリックされた答えの要素(liタグ)
+    const clickedAnwerElement = event.currentTarget;
+
+    console.log(clickedAnwerElement.dataset.answer);
+
+    // 選択した答え(A, B, C, D)
+    const selectedChoice = clickedAnwerElement.dataset.answer;
+
+    // 正しい答え(A, B, C, D)
+    answer = 'A';
+
+    // メッセージを入れる変数を用意
+    let message;
+
+    // カラーコードを入れる変数を用意
+    let answerColorCode;
+
+    // 答えを判定
+    if (selectedChoice == answer) {
+        // 答えが正しかった
+        message = '正解です！おめでとう';
+        answerColorCode = '';
+    } else {
+        // 間違った答えだった時
+        message = 'ざんねん！不正解です！'
+        answerColorCode = 'red';
+    }
+
+    alert(message);
+
+    // 間違った時だけ色が変わる
+    document.querySelector('#answer').style.color = answerColorCode;
+
+    // 答え全体を表示
+    document.querySelector('#section-correct-answer').style.display = 'block';
+}
+
