ed76b0d8ac0afe5e5d6b286e48c05d6c47115c56
DEGUCHIHIROKI <deguhiro4062@gmail.com>
2024-05-02 09:54:34 +0900

    phpファイルを作成

---
+++ Web/html/question.php
@@ -0,0 +1,3 @@
+<?php
+
+include __DIR__ .'/../templete/question..tpl.php';
---
+++ Web/templete/question..tpl.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="questions.js" defer></script>
+    <title>問題1 | Quiz</title>
+</head>
+<body>
+    <div id="main">
+        <div class="section">
+            <h1>Quiz</h1>
+            <h2>問題1</h2>
+            <p>ああああ</p>
+            <h3>選択肢</h3>
+            <ol class="choices" data-id="question1">
+                <li data-answer="A">あああ</li>
+                <li data-answer="B">いいい</li>
+                <li data-answer="C">ううう</li>
+                <li data-answer="D">えええ</li>
+            </ol>
+        </div>
+
+        <div id="section-correct-answer" class="section">
+            <h2>答え</h2>
+            <p>
+                <span id="answer">あああ</span><br>
+                解説解説
+            </p>
+        </div>
+
+        <div class="section">
+            <a href="index.html">戻る</a>
+        </div>
+    </div>
+</body>
+</html>
