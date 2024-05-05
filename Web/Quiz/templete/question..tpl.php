fbdd73ab2ef1baf80d6dfab489874d5b4164c851
DEGUCHIHIROKI <deguhiro4062@gmail.com>
2024-05-05 16:37:20 +0900

    共通箇所を変数に変更

--- Web/Quiz/templete/question..tpl.php
+++ Web/Quiz/templete/question..tpl.php
@@ -5,18 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <script src="questions.js" defer></script>
-    <title>問題1 | Quiz</title>
+    <title>問題<?php echo $id; ?> | Quiz</title>
 </head>
 <body>
     <div id="main">
         <div class="section">
             <h1>Quiz</h1>
-            <h2>問題1</h2>
+            <h2>問題<?php echo $id ?></h2>
             <p>
                 <?php echo $question; ?>
             </p>
             <h3>選択肢</h3>
-            <ol class="choices" data-id="question1">
+            <ol class="choices" data-id="<?php echo $id; ?>">
                 <li data-answer="A">あああ</li>
                 <li data-answer="B">いいい</li>
                 <li data-answer="C">ううう</li>
