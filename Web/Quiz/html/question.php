7e83b25b28f45c281573da1f639736ad81a3a577
DEGUCHIHIROKI <deguhiro4062@gmail.com>
2024-05-06 02:22:25 +0900

    idが異常だった場合、エラーページに飛ぶようにする

--- Web/Quiz/html/question.php
+++ Web/Quiz/html/question.php
@@ -2,8 +2,20 @@
 
 require __DIR__ . '/../lib/functions.php';
 
-$id = '3';
+$id = escape($_GET['id'] ?? '');
 $data = fetchById($id)[0];
+
+// !$dataがtrueだったらここでphpのスクリプトが終了する
+if (!$data) {
+    // HTTPレスポンスのヘッダを404にする
+    header('HTTP/1.1 404 Not Found');
+
+    // レスポンスの種類を指定する
+    header('Content-Type: text/html; charset=UTF-8');
+
+    include __DIR__ .'/../templete/404.tpl.php';
+    exit(0);
+}
 
 $formattedData = generateFormattedData($data);
 
