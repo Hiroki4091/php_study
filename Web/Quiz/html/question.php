8b7c0e46814bff25a3407fc29db7fa98ff1c6877
DEGUCHIHIROKI <deguhiro4062@gmail.com>
2024-05-05 23:46:52 +0900

    data.csvファイルのセキュリティの問題を解決

--- Web/Quiz/html/question.php
+++ Web/Quiz/html/question.php
@@ -5,18 +5,18 @@ require __DIR__ . '/../lib/functions.php';
 $id = '3';
 $data = fetchById($id)[0];
 
-$question = nl2br($data[1]);
+$question = nl2br(htmlspecialchars($data[1]));
 
 $answers = [
-    'A' => $data[2],
-    'B' => $data[3],
-    'C' => $data[4],
-    'D' => $data[5],
+    'A' => htmlspecialchars($data[2]),
+    'B' => htmlspecialchars($data[3]),
+    'C' => htmlspecialchars($data[4]),
+    'D' => htmlspecialchars($data[5]),
 ];
 
-$correctAnswer = strtoupper($data[6]);
+$correctAnswer = htmlspecialchars(strtoupper($data[6]));
 $correctAnswerValue = $answers[$correctAnswer];
-$explanation = nl2br($data[7]);
+$explanation = nl2br(htmlspecialchars($data[7]));
 
 // ファイルを読み込む
 include __DIR__ .'/../templete/question.tpl.php';
