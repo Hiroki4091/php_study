db10c55982483e6988e8ca514cbe56cb1e6dff12
DEGUCHIHIROKI <deguhiro4062@gmail.com>
2024-05-06 08:46:28 +0900

    index.phpから各問題に遷移可能、戻るボタンも実装

--- Web/Quiz/html/index.php
+++ Web/Quiz/html/index.php
@@ -2,28 +2,22 @@
 
 require __DIR__ . '/../lib/functions.php';
 
-$id = escape($_GET['id'] ?? '');
-$data = fetchById($id)[0];
+
+$dataList = fetchAll()[0];
 
 // !$dataがtrueだったらここでphpのスクリプトが終了する
-if (!$data) {
-    // HTTPレスポンスのヘッダを404にする
-    header('HTTP/1.1 404 Not Found');
-
-    // レスポンスの種類を指定する
-    header('Content-Type: text/html; charset=UTF-8');
-
+if (!$dataList) {
     loadTemplate('404');
-    exit(0);
 }
 
-$formattedData = generateFormattedData($data);
+$questions = [];
+foreach ($dataList as $data) {
+    $questions[] = generateFormattedData($data);
+}
 
 $assignData = [
-    'id' => $formattedData['id'],
-    'question' => $formattedData['question'],
-    'answers' => $formattedData['answers'],
+    'questions' => $questions,
 ];
 
 // ファイルを読み込む
-loadTemplate('question', $assignData);
+loadTemplate('index', $assignData);
--- Web/Quiz/html/question.php
+++ Web/Quiz/html/question.php
@@ -7,21 +7,16 @@ $data = fetchById($id)[0];
 
 // !$dataがtrueだったらここでphpのスクリプトが終了する
 if (!$data) {
-    // HTTPレスポンスのヘッダを404にする
-    header('HTTP/1.1 404 Not Found');
-
-    // レスポンスの種類を指定する
-    header('Content-Type: text/html; charset=UTF-8');
-
     loadTemplate('404');
-    exit(0);
 }
 
 $formattedData = generateFormattedData($data);
 
 $assignData = [
-    
+    'id' => $formattedData['id'],
+    'question' => $formattedData['question'],
+    'answers' => $formattedData['answers'],
 ];
 
 // ファイルを読み込む
-loadTemplate('index', $assignData);
+loadTemplate('question', $assignData);
